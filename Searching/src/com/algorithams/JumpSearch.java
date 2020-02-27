3a3f3780469fb98bd6b83b5b3d35003acb358d98
parsamPradeep <parzam77@gmail.com>
2020-02-27 23:46:52 +0530

    Jump Search

---
+++ Searching/src/com/algorithams/JumpSearch.java
@@ -0,0 +1,33 @@
+package com.algorithams;
+
+public class JumpSearch {
+	public static void jumpSearch(int arr[], int prev, int step, int key) {
+		if (step <= arr.length) {
+			if (key <= arr[step]) {
+				for (int i = prev; i <= step; i++) {
+					if (key == arr[i]) {
+						System.out.println("element found at :" + i);
+					}
+				}
+
+			} else {
+				prev = step;
+				step += step;
+				jumpSearch(arr, prev, step, key);
+			}
+
+		}else {
+			System.out.println("not fount");
+		}		
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int key = 71;
+		int step = (int) Math.floor(Math.sqrt(arr.length));
+		 jumpSearch(arr, 0, step, key);
+		
+
+	}
+
+}
