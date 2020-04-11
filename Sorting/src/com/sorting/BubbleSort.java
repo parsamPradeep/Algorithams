1535fc579104d10d10963b337b241df6fa63bf9c
parsamPradeep <parzam77@gmail.com>
2020-04-11 17:54:19 +0530

    optimised bubble sort

--- Sorting/src/com/sorting/BubbleSort.java
+++ Sorting/src/com/sorting/BubbleSort.java
@@ -2,14 +2,22 @@ package com.sorting;
 
 public class BubbleSort {
 	public static void bubbleSort(int arr[]) {
+		//for optimised bubble sort
+		boolean swap;
 		for(int i=0;i<arr.length;i++) {
+			swap=false;
 			for(int j=0;j<arr.length-i-1;j++) {
 				if(arr[j]>arr[j+1]) {
-					int temp=arr[j+1];
+					int temp=arr[j];
 					arr[j]=arr[j+1];
 					arr[j+1]=temp;
+					swap=true;
 				}
 			}
+			// If no elements are swapped in inner loop 
+			// then break
+			 if (swap == false) 
+	                break; 
 		}
 		for(int inx:arr) {
 			System.out.println(inx);
@@ -18,7 +26,7 @@ public class BubbleSort {
 	}
 
 	public static void main(String[] args) {
-		int arr[]= {20,21,45,67,77,87};
+		int arr[]= {20,2,45,6,77,8};
 		bubbleSort(arr);
 	}
 
