f2d4669459036d915bfc425cab5d45b43fcba98d
parsamPradeep <parzam77@gmail.com>
2020-04-11 17:33:14 +0530

    selction sort

--- Sorting/src/com/sorting/SelectionSort.java
+++ Sorting/src/com/sorting/SelectionSort.java
@@ -2,14 +2,16 @@ package com.sorting;
 
 public class SelectionSort {
 	public static void selectionSorting(int arr[]) {
-		for(int i=0;i<arr.length;i++) {
-			for(int j=0;j<arr.length-1;j++) {
-				if(arr[i]<arr[j]) {
-					int temp=arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
+		for(int i=0;i<arr.length-1;i++) {
+			int sel_inx=i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[j]<arr[sel_inx]) {
+					sel_inx=j;
 				}
 			}
+			int temp=arr[i];
+			arr[i]=arr[sel_inx];
+			arr[sel_inx]=temp;
 		}
 		for(int inx:arr) {
 			System.out.println(inx);
